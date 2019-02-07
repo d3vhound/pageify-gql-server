dbc794291a32cf686266ce12820efd17dec597e5
Devion Villegas <devion.villegas@ttu.edu>
2019-02-06 20:42:07 -0600

    update

--- src/resolvers/post.js
+++ src/resolvers/post.js
@@ -25,6 +25,32 @@ const allCategories = [
 	"all"
 ]
 
+function linkify(str, hashtags){
+  // order matters
+  var re = [
+      "\\b((?:https?|ftp)://[^\\s\"'<>]+)\\b",
+      "\\b(www\\.[^\\s\"'<>]+)\\b",
+      "\\b(\\w[\\w.+-]*@[\\w.-]+\\.[a-z]{2,6})\\b", 
+      "#([a-z0-9]+)"];
+  re = new RegExp(re.join('|'), "gi");
+
+  return str.replace(re, function(match, url, www, mail, twitler){
+      if(url)
+          return "<a href=\"" + url + "\">" + url + "</a>";
+      if(www)
+          return "<a href=\"http://" + www + "\">" + www + "</a>";
+      if(mail)
+          return "<a href=\"mailto:" + mail + "\">" + mail + "</a>";
+      if (twitler) {
+        const hashtag = hashtags.find(x => x.dataValues.hashtag === twitler)
+        return `<span>Regular text <a (click)='navigateToHashtag("/tags/${twitler};id=${hashtag.id}")' >${twitler}</a></span>`
+      }
+
+      // shouldnt get here, but just in case
+      return match;
+  });
+}
+
 function storeUpload({ stream, mimetype, s3,}) {
 	return new Promise((resolve, reject) => {
 		const uuidFilename = uuidv4()
@@ -1050,6 +1076,13 @@ export default {
 	},
 
 	Post: {
+    text: async (post, args, { models }) => {
+      if (post.hashtags.length) {
+        return linkify(text, post.hashtags)
+      }
+
+      return text
+    },
 		// user: async (post, args, { models, loaders }) => {
 		// 	// return await models.User.findById(post.userId)
 		// 	return await loaders.user.load(post.userId)
